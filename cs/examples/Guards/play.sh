c0e3114591755ed5f0b961c6de799b22d52b06b1
kkolyan <kkolyankkolyan@gmail.com>
2025-05-14 03:48:33 +0300

    fix and refactor Guards/play.sh and related Rider project

--- cs/examples/Guards/play.sh
+++ cs/examples/Guards/play.sh
@@ -1,11 +1,40 @@
 #!/usr/bin/env bash
 cd "$(dirname "$0")"
 set -e
+
+# Go to project root
+
 cd ../../..
-cargo build -p fyrox-c
-mkdir -p cs/examples/Guards/bin/Debug/net8.0
-cp target/debug/*fyrox_c.* cs/examples/Guards/bin/Debug/net8.0
+
+# cleanup to detect regression asap
+rm -rf cs/examples/Guards/bin/Debug/net8.0/*.dll
+rm -rf cs/examples/Guards/bin/Debug/net8.0/*.exe
+rm -rf cs/examples/Guards/bin/Debug/net8.0/*.runtimeconfig.json
+
+# build C# parts of Fyrox Lite
+
 cd cs/FyroxLite
 dotnet build
-cd ../examples/Guards
-RUST_BACKTRACE=full dotnet run
+cd ../..
+
+# build the C# game code
+
+cd cs/examples/Guards
+dotnet build
+cd ../../..
+
+cp cs/FyroxLite/FyroxLite/bin/Debug/net8.0/FyroxLite.dll cs/examples/Guards/bin/Debug/net8.0/
+
+# build Rust parts of Fyrox Lite
+
+# fyroxed-c is built too to avoid rebuilds between edit and play (not sure it works that way, need to check)
+RUSTFLAGS='-C prefer-dynamic=yes' cargo build -p fyrox-c -p fyroxed-c
+cp target/debug/fyrox_c.dll cs/examples/Guards/bin/Debug/net8.0/
+cp target/debug/deps/fyrox_dylib*.dll cs/examples/Guards/bin/Debug/net8.0/
+./get_rust_std.sh cs/examples/Guards/bin/Debug/net8.0/
+
+# Return to game folder
+
+cd cs/examples/Guards
+
+RUST_BACKTRACE=1 ../../../cs/examples/Guards/bin/Debug/net8.0/Guards.exe
