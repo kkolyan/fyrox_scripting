f89d039a500c1a641928cc9c62bfa3dab89f1211
kkolyan <kkolyankkolyan@gmail.com>
2025-06-08 03:05:48 +0300

    www

--- tools/src/bin/mdgen.rs
+++ tools/src/bin/mdgen.rs
@@ -8,7 +8,7 @@ use std::fs;
 fn main() {
     let domain = tools::get_fyrox_lite_domain();
     let cs_domain = generate_cs_defined_domain();
-    let target_dir = "docs/src/scripting_api_cs";
+    let target_dir = "www/src/scripting_api_cs";
     let code_base = md_gen_lib::generate_md::generate_md(&domain, &cs_domain, Naming::Cs);
     let _ = fs::remove_dir_all(target_dir);
     code_base.write_md(target_dir);
@@ -17,9 +17,9 @@ fn main() {
     let mut summary_section = String::new();
     generate_summary_section("", &mut breadcrumb, &code_base.children.values().collect_vec(), &mut summary_section);
 
-    let summary_template = fs::read_to_string("docs/src/SUMMARY.template.md").unwrap();
+    let summary_template = fs::read_to_string("www/src/SUMMARY.template.md").unwrap();
     let summary = summary_template.replace("%GENERATED_SECTION%", &summary_section);
-    fs::write("docs/src/SUMMARY.md", summary).unwrap();
+    fs::write("www/src/SUMMARY.md", summary).unwrap();
 }
 
 fn generate_summary_section(
