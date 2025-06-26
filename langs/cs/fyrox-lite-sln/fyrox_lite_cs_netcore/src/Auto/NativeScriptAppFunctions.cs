1033b1b0de759291b58a4f9417e2bc55e58d9423
kkolyan <kkolyankkolyan@gmail.com>
2025-06-26 20:17:57 +0300

    gen

--- langs/cs/fyrox-lite-sln/fyrox_lite_cs_netcore/src/Auto/NativeScriptAppFunctions.cs
+++ langs/cs/fyrox-lite-sln/fyrox_lite_cs_netcore/src/Auto/NativeScriptAppFunctions.cs
@@ -15,84 +15,72 @@ internal unsafe partial struct NativeScriptAppFunctions {
     private IntPtr _init_scripts_metadata;
     internal FyroxNativeGlobal.InitScriptsMetadata init_scripts_metadata
     {
-
         get => Marshal.GetDelegateForFunctionPointer<FyroxNativeGlobal.InitScriptsMetadata>(_init_scripts_metadata);
         set => _init_scripts_metadata = Marshal.GetFunctionPointerForDelegate(value);
     }
     private IntPtr _get_scripts_metadata;
     internal FyroxNativeGlobal.GetScriptsMetadata get_scripts_metadata
     {
-
         get => Marshal.GetDelegateForFunctionPointer<FyroxNativeGlobal.GetScriptsMetadata>(_get_scripts_metadata);
         set => _get_scripts_metadata = Marshal.GetFunctionPointerForDelegate(value);
     }
     private IntPtr _on_init;
     internal FyroxNativeGlobal.NodeOnInit on_init
     {
-
         get => Marshal.GetDelegateForFunctionPointer<FyroxNativeGlobal.NodeOnInit>(_on_init);
         set => _on_init = Marshal.GetFunctionPointerForDelegate(value);
     }
     private IntPtr _on_start;
     internal FyroxNativeGlobal.NodeOnStart on_start
     {
-
         get => Marshal.GetDelegateForFunctionPointer<FyroxNativeGlobal.NodeOnStart>(_on_start);
         set => _on_start = Marshal.GetFunctionPointerForDelegate(value);
     }
     private IntPtr _on_deinit;
     internal FyroxNativeGlobal.NodeOnDeinit on_deinit
     {
-
         get => Marshal.GetDelegateForFunctionPointer<FyroxNativeGlobal.NodeOnDeinit>(_on_deinit);
         set => _on_deinit = Marshal.GetFunctionPointerForDelegate(value);
     }
     private IntPtr _on_update;
     internal FyroxNativeGlobal.NodeOnUpdate on_update
     {
-
         get => Marshal.GetDelegateForFunctionPointer<FyroxNativeGlobal.NodeOnUpdate>(_on_update);
         set => _on_update = Marshal.GetFunctionPointerForDelegate(value);
     }
     private IntPtr _on_message;
     internal FyroxNativeGlobal.NodeOnMessage on_message
     {
-
         get => Marshal.GetDelegateForFunctionPointer<FyroxNativeGlobal.NodeOnMessage>(_on_message);
         set => _on_message = Marshal.GetFunctionPointerForDelegate(value);
     }
     private IntPtr _on_game_init;
     internal FyroxNativeGlobal.GameOnInit on_game_init
     {
-
         get => Marshal.GetDelegateForFunctionPointer<FyroxNativeGlobal.GameOnInit>(_on_game_init);
         set => _on_game_init = Marshal.GetFunctionPointerForDelegate(value);
     }
     private IntPtr _on_game_update;
     internal FyroxNativeGlobal.GameOnUpdate on_game_update
     {
-
         get => Marshal.GetDelegateForFunctionPointer<FyroxNativeGlobal.GameOnUpdate>(_on_game_update);
         set => _on_game_update = Marshal.GetFunctionPointerForDelegate(value);
     }
     private IntPtr _create_script_instance;
     internal FyroxNativeGlobal.CreateScriptInstance create_script_instance
     {
-
         get => Marshal.GetDelegateForFunctionPointer<FyroxNativeGlobal.CreateScriptInstance>(_create_script_instance);
         set => _create_script_instance = Marshal.GetFunctionPointerForDelegate(value);
     }
     private IntPtr _dispose_message;
     internal FyroxNativeGlobal.DisposeMessage dispose_message
     {
-
         get => Marshal.GetDelegateForFunctionPointer<FyroxNativeGlobal.DisposeMessage>(_dispose_message);
         set => _dispose_message = Marshal.GetFunctionPointerForDelegate(value);
     }
     private IntPtr _dispose_script;
     internal FyroxNativeGlobal.DisposeScript dispose_script
     {
-
         get => Marshal.GetDelegateForFunctionPointer<FyroxNativeGlobal.DisposeScript>(_dispose_script);
         set => _dispose_script = Marshal.GetFunctionPointerForDelegate(value);
     }
